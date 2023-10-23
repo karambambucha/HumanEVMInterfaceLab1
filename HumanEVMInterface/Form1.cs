a7a1c15a42b99761375665a0fa94c7cffad2f9c9
olegspace <olezhka_revin@mail.ru>
2023-10-23 12:18:13 +0700

    Add base imitation

--- HumanEVMInterface/Form1.cs
+++ HumanEVMInterface/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Windows.Forms;
 
@@ -15,8 +16,10 @@ namespace HumanEVMInterface
         public int RoutesNumber; //колво маршрутов
         public int RouteLength; //длина маршрута
         public int RouteTypeNumbers; //кол-во вершин
+        public List<int> RoutesFinish1 = new List<int>();
+        public List<int> RoutesFinish2 = new List<int>();
         public double[,] ProbabiltyMatrix;
-        public double[,] ProbabiltyMatrix2; 
+        public double[,] ProbabiltyMatrix2;
         public double[,] StepTimes; //время ходьбы через каждую вершину 1 подтемы
         public double[,] StepTimes2; //время ходьбы через каждую вершину 2 подтемы
         public double[] SubthemeProbablity; //вероятность выбора подтемы
@@ -69,7 +72,7 @@ namespace HumanEVMInterface
                     return false;
             return true;
         }
-        bool CheckMatrix(double[,] matrix)
+        bool CheckMatrix(double[,] matrix, int numberSubTheme)
         {
             int rowCount = matrix.GetLength(0);
             int colCount = matrix.GetLength(1);
@@ -78,18 +81,37 @@ namespace HumanEVMInterface
             double[] colSums = new double[colCount];
 
             for (int row = 0; row < rowCount; row++)
+            {
                 for (int col = 0; col < colCount; col++)
                 {
                     rowSums[row] += matrix[row, col];
                     colSums[col] += matrix[row, col];
                 }
+            }
+
+            for (int i = 0; i < rowSums.Length; i++)
+            {
+                if (rowSums[i] == 0)
+                {
+                    if (numberSubTheme == 1) RoutesFinish1.Add(i + 1);
+                    else RoutesFinish2.Add(i);
+                }
+            }
+
 
             for (int i = 0; i < rowCount; i++)
-                if (rowSums[i] != 1)
+            {
+                if (rowSums[i] == 0)
+                {
+                    if (numberSubTheme == 1) RoutesFinish1.Add(i + 1); // Определение конечных вершин
+                    else RoutesFinish2.Add(i);
+                }
+                else if (rowSums[i] != 1)
                     return false;
+            }
 
             for (int j = 0; j < colCount; j++)
-                if (colSums[j] != 1)
+                if (colSums[j] != 1 && colSums[j] != 0)
                     return false;
 
             return true;
@@ -101,7 +123,10 @@ namespace HumanEVMInterface
             {
                 SubthemeProbablity = new double[2];
                 StepTimes = new double[2, (int)RouteTypeNumberNumeric.Value];
+                StepTimes2 = new double[2, (int)RouteTypeNumberNumeric2.Value];
                 ProbabiltyMatrix = new double[(int)RouteTypeNumberNumeric.Value, (int)RouteTypeNumberNumeric.Value];
+                ProbabiltyMatrix2 = new double[(int)RouteTypeNumberNumeric2.Value, (int)RouteTypeNumberNumeric2.Value];
+
                 if (MistakeTypeComboBox.Text == "")
                 {
                     throw new Exception("Тип ошибки не выбран!");
@@ -111,30 +136,30 @@ namespace HumanEVMInterface
                     {
                         if (!double.TryParse((string)StepTimesDataGridView.Rows[row].Cells[col].Value, out double cellValue))
                             throw new Exception("Матрица длины шагов не полностью записана!");
-                        StepTimes[col, row] = cellValue;
+                        StepTimes[row, col] = cellValue;
                     }
                 for (int row = 0; row < ProbabilityMatrixDataGridView.Rows.Count; row++)
                     for (int col = 0; col < ProbabilityMatrixDataGridView.Columns.Count; col++)
                     {
                         if (!double.TryParse((string)ProbabilityMatrixDataGridView.Rows[row].Cells[col].Value, out double cellValue))
                             throw new Exception("Матрица вероятностей не полностью записана!");
-                        ProbabiltyMatrix[col, row] = cellValue;
+                        ProbabiltyMatrix[row,col] = cellValue;
                     }
                 for (int row = 0; row < StepTimesDataGridView2.Rows.Count; row++)
                     for (int col = 0; col < StepTimesDataGridView2.Columns.Count; col++)
                     {
                         if (!double.TryParse((string)StepTimesDataGridView2.Rows[row].Cells[col].Value, out double cellValue))
                             throw new Exception("Матрица длины шагов не полностью записана!");
-                        StepTimes2[col, row] = cellValue;
+                        StepTimes2[row, col] = cellValue;
                     }
                 for (int row = 0; row < ProbabilityMatrixDataGridView2.Rows.Count; row++)
                     for (int col = 0; col < ProbabilityMatrixDataGridView2.Columns.Count; col++)
                     {
                         if (!double.TryParse((string)ProbabilityMatrixDataGridView2.Rows[row].Cells[col].Value, out double cellValue))
                             throw new Exception("Матрица вероятностей не полностью записана!");
-                        ProbabiltyMatrix2[col, row] = cellValue;
+                        ProbabiltyMatrix2[row, col] = cellValue;
                     }
-                if (!CheckMatrix(ProbabiltyMatrix) || !CheckMatrix(ProbabiltyMatrix2))
+                if (!CheckMatrix(ProbabiltyMatrix, 1) || !CheckMatrix(ProbabiltyMatrix2, 2))
                     throw new Exception("Суммы строк и столбцов НЕ равны 1.");
                 if (!CheckDiagonalZero(ProbabiltyMatrix) || !CheckDiagonalZero(ProbabiltyMatrix2))
                     throw new Exception("Не все диагональные ячейки равны 0.");
@@ -159,11 +184,123 @@ namespace HumanEVMInterface
                         break;
                 }
                 SelectionSize = (int)SelectionSizeNumeric.Value;
-            }
+
+                Imatation();
+            }            
+
             catch (Exception exp)
             {
                 MessageBox.Show(exp.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void Imatation()
+        {
+            Random random = new Random();
+            double randomNumber = random.NextDouble();
+
+            bool isFirstSubtheme = false;
+
+            if (randomNumber < SubthemeProbablity[0])
+            {
+                isFirstSubtheme = true;
+            }
+
+            double cumulativeProbability = 0.0;
+
+            double totalTime = 0;
+
+            if (isFirstSubtheme)
+            {
+                for (int i = 0; i < ProbabiltyMatrix.GetLength(0); )
+                {
+                    if (RoutesFinish1.Contains(i))
+                    {
+                        if (StepTimes[i, 1] == 0)
+                        {
+                            totalTime += StepTimes[i, 0];
+                        }
+                        else
+                        {
+                            totalTime += StepTimes[i, 0] + random.NextDouble() * (StepTimes[i, 1] - StepTimes[i, 0]);
+                        }
+                        break;
+                    }
+                    for (int j = 0; j < ProbabiltyMatrix.GetLength(1); j++)
+                    {
+                        if (ProbabiltyMatrix[i, j] != 0)
+                        {
+                            cumulativeProbability += ProbabiltyMatrix[i, j];
+                            if (randomNumber < cumulativeProbability)
+                            {
+                                //int nextRoute = ++j;
+                                
+                                // Прибавить время прохождения  вершины
+                                if (StepTimes[i, 1] == 0)
+                                {
+                                    totalTime += StepTimes[i, 0];
+                                }
+                                else
+                                {
+                                    totalTime += StepTimes[i, 0] + random.NextDouble() * (StepTimes[i, 1] - StepTimes[i, 0]);
+                                }
+                                i = j;
+                                break;
+                            }
+                        }
+                    }
+
+                    // |0   0.3 0.3 0.4 0  |
+                    // |0   0   0   0.2 0.8|
+                    // |0   0   0   0   1  |
+                    // |0   0   0   0   1  |
+                    // |0   0   0   0   0  |
+                }
+            }
+            else
+            {
+                for (int i = 0; i < ProbabiltyMatrix2.GetLength(0); )
+                {
+                    if (RoutesFinish2.Contains(i))
+                    {
+                        if (StepTimes2[i, 1] == 0)
+                        {
+                            totalTime += StepTimes2[i, 0];
+                        }
+                        else
+                        {
+                            totalTime += StepTimes2[i, 0] + random.NextDouble() * (StepTimes2[i, 1] - StepTimes2[i, 0]);
+                        }
+                        break;
+                    }
+                    for (int j = 0; j < ProbabiltyMatrix2.GetLength(1); j++)
+                    {
+                        if (ProbabiltyMatrix[i, j] != 0)
+                        {
+                            cumulativeProbability += ProbabiltyMatrix2[i, j];
+                            if (randomNumber < cumulativeProbability)
+                            {
+                                
+                                if (StepTimes[i, 1] == 0)
+                                {
+                                    totalTime += StepTimes2[i, 0];
+                                }
+                                else
+                                {
+                                    totalTime += StepTimes2[i, 0] + random.NextDouble() * (StepTimes2[i, 1] - StepTimes2[i, 0]);
+                                }
+
+                                //int nextRoute = j +;
+                                i = j;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            MessageBox.Show(totalTime.ToString());
+            
         }
 
         private void ProbabilityMatrixDataGridView_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
